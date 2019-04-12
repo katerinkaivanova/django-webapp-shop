8e536d5b318a1810a5be5ba9c7e8dbdf4c28e4e4
Ekaterina Ivanova <ivanova.ecaterina@yandex.ru>
2019-04-12 16:22:51 +0300

    lesson-6: hot deal product and product page

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,8 @@
-# Create your views here.
-
 from django.shortcuts import render, get_object_or_404
 from mainapp.models import ProductCategory, Product
 from basketapp.models import Basket
+
+import random
 
 
 links_menu = [
@@ -14,11 +14,31 @@ links_menu = [
 ]
 
 
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category). \
+                        exclude(pk=hot_product.pk)[:3]
+
+    return same_products
+
+
 def main_view(request):
 
     products = Product.objects.all()[:4]
     trending_products = Product.objects.all()[:6]
-    basket = Basket.objects.filter(user=request.user)
+    basket = get_basket(request.user)
 
     my_context = {
         'title': links_menu[0]['title'],
@@ -33,7 +53,7 @@ def main_view(request):
 def products_view(request, pk=None):
 
     links_categories = ProductCategory.objects.all()
-    basket = Basket.objects.filter(user=request.user)
+    basket = get_basket(request.user)
 
     if pk is not None:
         if pk == 0:
@@ -55,18 +75,34 @@ def products_view(request, pk=None):
         return render(request, 'mainapp/products_list.html', my_context)
 
     else:
-        same_products = Product.objects.all()[:12]
+        hot_product = get_hot_product()
+        same_products = get_same_products(hot_product)
 
         my_context = {
             'title': links_menu[1]['title'],
             'links_menu': links_menu,
             'links_categories': links_categories,
+            'hot_product': hot_product,
             'same_products': same_products,
-            'basket': basket
+            'basket': basket,
         }
 
         return render(request, 'mainapp/products.html', my_context)
 
+
+def product_view(request, pk):
+
+    links_categories = ProductCategory.objects.all()
+
+    my_context = {
+        'title': links_menu[1]['title'],
+        'links_menu': links_menu,
+        'links_categories': links_categories,
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+    }
+
+    return render(request, 'mainapp/product.html', my_context)
 
 def history_view(request):
 
@@ -90,7 +126,7 @@ def showroom_view(request):
 
 def contact_view(request):
 
-    basket = Basket.objects.filter(user=request.user)
+    basket = get_basket(request.user)
 
     my_context = {
         'title': links_menu[4]['title'],
